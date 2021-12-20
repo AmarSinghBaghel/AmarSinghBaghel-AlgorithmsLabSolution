2688791a7931907d6fccd50d29cf82f6d8675dfb
AMAR SINGH BAGHEL <71514134+AmarSinghBaghel@users.noreply.github.com>
2021-12-20 20:26:28 +0530

    Updated

--- Lab 2 - Algorithms Q2/src/com/greatlearning/Driver/DriverQ2.java
+++ Lab 2 - Algorithms Q2/src/com/greatlearning/Driver/DriverQ2.java
@@ -6,20 +6,15 @@ import com.greatlearning.services.*;
 public class DriverQ2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		int n;
+		
+		int n;                                        //Declaring array length variable as n.
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the size of currency denominations");
 		n = sc.nextInt();
 		int arr[] = null ;		
-		arr = ArrayCreator.arry(n);
-		//system.out.println("Enter the  currency denominations are");
-		//display.PrintArr(arr,n);		
-		MergeSort.mergeSort(arr, 0, arr.length-1);
-		//System.out.println("The  sorted currency denominations are");
-		//display.PrintArr(arr,n);
-		MinNoteCal.minNote(arr);
+		arr = ArrayCreator.arry(n);	             //Array creator method call of array length n , as per  input entered by client.	
+		MergeSort.mergeSort(arr, 0, arr.length-1);  //Merge sort method call to sort input array in descending order.
+		MinNoteCal.minNote(arr);                    //Minimum note calculator method call.
 	}
 
 }
